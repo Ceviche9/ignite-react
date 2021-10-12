aeecf4b631564f26f1f6837e3072027236c3a7e9
Tundê <ayotunde_sales@hotmail.com>
2021-10-12 15:56:03 -0300

    feat/chapter_2: application structure

--- 02-dt-money/src/App.tsx
+++ 02-dt-money/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello world</h1>
     </div>
   );
