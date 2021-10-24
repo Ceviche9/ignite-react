ac1bcce1e9a61ade7a149e053979081fa144a5ce
Tundê <ayotunde_sales@hotmail.com>
2021-10-24 11:20:54 -0300

    feat/chapter_3/: add next-auth

--- 03-devnews/src/components/SingInButton/index.tsx
+++ 03-devnews/src/components/SingInButton/index.tsx
@@ -1,25 +1,31 @@
-import { useState } from 'react'
 import { FaGithub } from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
+
+import {signIn, signOut, useSession} from 'next-auth/client'
 
 import styles from './styles.module.scss'
  
 export const SingInButton = () => {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true)
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
+      type="button"
       className={styles.singInButton}
-      type="button">
+      onClick={() => signOut()}
+      >
       <FaGithub color="#04d361" />
-      Tundê Cavalcante
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) 
   : (
     <button 
+      type="button"
       className={styles.singInButton}
-      type="button">
+      // Qual o tipo de autenticação
+      onClick={() => signIn('github')}
+      >
       <FaGithub color="#eba417" />
       SingIn with GIthub  
   </button>
