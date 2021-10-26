dcfac3502403fdf04e728d18e9b0e286a21ee197
Tundê <ayotunde_sales@hotmail.com>
2021-10-26 20:41:37 -0300

    feat/api: add stripe payment

--- 03-devnews/src/components/SubscribeButton/index.tsx
+++ 03-devnews/src/components/SubscribeButton/index.tsx
@@ -1,11 +1,40 @@
+import { useSession, signIn } from 'next-auth/client'
 import { SubscribeButtonProps } from '../../protocols/componentsProtocols'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+
+
 import styles from './styles.module.scss'
 
 export const SubscribeButton = ({priceId}: SubscribeButtonProps) => {
+  const [session] = useSession()
+  
+  const handlesubscribe = async() => {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+    // Criação da checkout session
+    try{
+      const response = await api.post('/subscribe')
+
+      const {sessionId} = response.data
+
+      const stripe = await getStripeJs()
+
+      stripe.redirectToCheckout({sessionId})
+
+
+    }catch(err){
+      alert(err.message)
+    }
+  }
+
   return(
     <button
-      className={styles.subscribeButton}
       type="button"
+      className={styles.subscribeButton}
+      onClick={handlesubscribe}
     >
       Subscribe Now
     </button>
