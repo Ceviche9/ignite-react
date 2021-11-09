14007571006d2533fa812cef1427ffebb3537675
Tundê <ayotunde_sales@hotmail.com>
2021-11-09 16:01:59 -0300

    feat/chapter_4_home: add sing in styles

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,5 +1,64 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-   <h1>Hello world</h1>
+    <Flex 
+      w="100vw" 
+      h="100vh"
+      align="center"
+      justify="center" 
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+          <FormLabel htmlFor="email">Email:</FormLabel>
+            <Input 
+              name="email" 
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password:</FormLabel>
+            <Input 
+              name="password" 
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button 
+          type="submit" 
+          mt="6" 
+          bgColor="pink.500"
+          size="lg"
+          _hover={{
+            bgColor: "pink.600"
+          }}
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
