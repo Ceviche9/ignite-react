0b34d07b325990078c0d516a77d1ba1c07006276
Tundê <ayotunde_sales@hotmail.com>
2021-11-19 12:07:15 -0300

    feat/chapter_4_signin: add validation

--- 04-dashgo/src/components/Form/input.tsx
+++ 04-dashgo/src/components/Form/input.tsx
@@ -1,14 +1,17 @@
-import {FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps} from '@chakra-ui/react'
+import {FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps, FormErrorMessage} from '@chakra-ui/react'
 import { forwardRef, ForwardRefRenderFunction } from 'react'
+import {FieldError} from "react-hook-form"
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError
 }
 
-export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({name, label, ...rest}: InputProps, ref) => {
+export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({name, label, error = null, ...rest}: InputProps, ref) => {
   return(
-    <FormControl>
+    // Verificando se existe alguma mensagem de erro.
+    <FormControl isInvalid={!!error}>
     {!! label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput 
         name={name}
@@ -23,6 +26,11 @@ export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> =
         ref={ref}
         {...rest}
       />
+      {!!error && (
+        <FormErrorMessage>
+          {error.message}
+        </FormErrorMessage>
+      )}
     </FormControl>
   )
 }
--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,15 +1,29 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
 import {SubmitHandler, useForm} from "react-hook-form"
 
+import * as yup from "yup"
+import {yupResolver} from "@hookform/resolvers/yup/dist/yup"
+
 import {Input} from '../components/Form/input'
 
 type SingInFormData = {
   email: string
   password: string
 }
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('Campo email obrigatório').email('Email inválido'),
+  password: yup.string().required('Campo senha obrigatório')
+})
+
 // const {errors} = formState
 export default function SingIn() {
-  const {register, handleSubmit, formState} = useForm()
+  const {register, handleSubmit, formState} = useForm({
+    // Para fazer a validação do formulário
+    resolver: yupResolver(signInFormSchema)
+  })
+  // Para pegar as mensagens de errors geradas
+  const {errors} = formState
 
   const handleSingIn: SubmitHandler<SingInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000))
@@ -35,8 +49,21 @@ export default function SingIn() {
         onSubmit={handleSubmit(handleSingIn)}
       >
         <Stack spacing="4">
-          <Input type="email" name="email" label="Email" {...register("email")}/>
-          <Input type="password" name="password" label="Password" {...register("password")} />
+          <Input 
+            type="email" 
+            name="email" 
+            label="Email" 
+            // Para fazer a validação do campo
+            {...register("email")}
+            error={errors.email}
+          />
+          <Input 
+            type="password" 
+            name="password" 
+            label="Password" 
+            {...register("password")}
+            error={errors.password}
+          />
         </Stack>
         <Button 
           type="submit" 
