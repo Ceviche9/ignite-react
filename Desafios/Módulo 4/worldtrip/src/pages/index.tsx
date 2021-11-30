6259c647ce2c2e239417276437b26cf82f8117fc
Tundê <ayotunde_sales@hotmail.com>
2021-11-29 23:31:50 -0300

    feat/challenger_4_home: add spacing

--- Desafios/Módulo 4/worldtrip/src/pages/index.tsx
+++ Desafios/Módulo 4/worldtrip/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {Flex, Text, Box, Heading, Divider} from "@chakra-ui/react"
+import {Flex, Text, Box, Heading, Divider, Stack} from "@chakra-ui/react"
 
 import { Header } from "../components/Header"
 import { Options } from "../components/Options"
@@ -23,81 +23,88 @@ export default function Home({continents}: HomeProps) {
       h="100vh"
       mb={25}
     >
-      <Header/>
-      <Flex
-        align="center"
-        justify="space-between"
-        px={36}
-        w="100%"
-        h={335}
-        backgroundImage="url('/images/Background.svg')"
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-      >
-        <Box>
-          <Box
-            color="gray.50"
-          >
-            <Heading
-              fontWeight="500"
-            >
-              5 Continentes,
-            </Heading>
-            <Heading
-              fontWeight="500"
-            >
-              infinitas possibilidades.
-            </Heading>
-          </Box>
-          <Text
-            mt="3"
-            color="gray.50"
-          >
-          Chegou a hora de tirar do papel a viagem que você <br/> sempre sonhou. 
-          </Text>
-        </Box>
-        <Box
-          mt={32}
-        >
-          <Image 
-            width={417}
-            height={270}
-            src="/images/Airplane.svg" 
-            alt="Avião"
-          />
-        </Box>
-      </Flex>
-      <Options />
-      <Divider my="2" borderColor="gray.900" w="16" alignSelf="center" border="23"/>
-      <Heading
-        mt="14"
-        color="gray.600"
-        fontWeight="500"
-        alignSelf="center"
-      >
-        Vamos nessa ?
-      </Heading>
-      <Heading
-        mt="2"
-        color="gray.600"
-        fontWeight="500"
-        alignSelf="center"
-      >
-        Então escolha seu continente
-      </Heading>
-      <Box
-        paddingBottom={25}
+      <Header/> 
+      <Stack
+        spacing="24"
       >
         <Flex
-          maxWidth={1310}
-          paddingX="10"
-          mt="6"
-          mx="auto"
+          align="center"
+          justify="space-between"
+          px={36}
+          w="100%"
+          h={335}
+          backgroundImage="url('/images/Background.svg')"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
         >
-          <SwiperCarrousel data={continents}
-          />
+          <Box>
+            <Box
+              color="gray.50"
+            >
+              <Heading
+                fontWeight="500"
+              >
+                5 Continentes,
+              </Heading>
+              <Heading
+                fontWeight="500"
+              >
+                infinitas possibilidades.
+              </Heading>
+            </Box>
+            <Text
+              mt="3"
+              color="gray.50"
+            >
+            Chegou a hora de tirar do papel a viagem que você <br/> sempre sonhou. 
+            </Text>
+          </Box>
+          <Box
+            mt={32}
+          >
+            <Image 
+              width={417}
+              height={270}
+              src="/images/Airplane.svg" 
+              alt="Avião"
+            />
+          </Box>
         </Flex>
-      </Box>
+
+        <Options />
+        
+        <Divider my="2" borderColor="gray.900" w="16" alignSelf="center" border="23"/>
+        
+        <Stack>
+          <Heading
+            color="gray.600"
+            fontWeight="500"
+            alignSelf="center"
+          >
+            Vamos nessa ?
+          </Heading>
+          <Heading
+            color="gray.600"
+            fontWeight="500"
+            alignSelf="center"
+          >
+            Então escolha seu continente
+          </Heading>
+          <Box
+            paddingBottom={25}
+          >
+            <Flex
+              maxWidth={1310}
+              paddingX="10"
+              mt="6"
+              mx="auto"
+            >
+              <SwiperCarrousel data={continents}
+              />
+            </Flex>
+          </Box>
+        </Stack>
+      </Stack>
     </Flex>
   )
 }
